bbb981de6e37e7491b84f9a27e042a72de3d9626
Lamberto <lamberto@RyRII>
2015-05-22 17:09:54 -0600

    Se agregaron mas clases

---
+++ interfacesimple/Fecha.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionavanzadamarzojulio2015.interfacesimple;
+
+/**
+ *
+ * @author lamberto
+ */
+public class Fecha {
+    int dia, mes, anyio;
+    public Fecha(int d, int m, int a){
+        dia = d; mes = m; anyio = a;
+    }
+    public static void main(String[] args) {
+        Fecha F = new Fecha(22, 5, 2015);
+        MuestraFechaNEspanyiol MFNE = new MuestraFechaNEspanyiol();
+        MFNE.mostrar(F);
+    }
+}
---
+++ interfacesimple/MuestraFechaNEspanyiol.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionavanzadamarzojulio2015.interfacesimple;
+
+public class MuestraFechaNEspanyiol implements Shower{
+    int pnm[] = {0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0};
+    String NdD[] = {"Lunes", "Martes", "Miercoles", 
+        "Jueves", "Viernes", "Sabado", "Domingo"};
+    public void mostrar(Fecha f){
+        System.out.println( NdD[(f.dia + 7 - pnm[f.mes - 1]) % 7] + ", del mes "
+                + f.mes + " del anyio " + f.anyio);
+    }
+}
