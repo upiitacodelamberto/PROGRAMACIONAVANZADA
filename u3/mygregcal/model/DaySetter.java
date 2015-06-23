be472cac9e82331f9b007e08af331e77c4bbd3aa
Lamberto <lamberto@RyRII>
2015-06-23 17:04:55 -0600

    Commit de 2015.06.23

---
+++ u3/mygregcal/model/DaySetter.java
@@ -0,0 +1,10 @@
+package u3.mygregcal.model;
+
+public interface DaySetter {
+
+    /**
+     * Establecer el nombre del dia de la fecha f.
+     * @param f:Fecha 
+     */
+    public void setNomDD(Fecha f);
+}
