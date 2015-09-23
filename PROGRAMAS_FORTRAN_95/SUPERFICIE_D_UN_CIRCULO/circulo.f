01b3c7617cf89b71664f29bbffa82c965e03c20b
Lamberto <lamberto@RyRII>
2015-09-22 19:14:04 -0600

    (2015.09.22) Se agrega el directorio PROGRAMAS_FORTRAN_95

---
+++ PROGRAMAS_FORTRAN_95/SUPERFICIE_D_UN_CIRCULO/circulo.f
@@ -0,0 +1,24 @@
+program circulo
+!
+!programa para calcular areas de circulos
+!
+implicit none
+real::radio, superficie
+parameter (pi=3.1415926)
+!
+!visualizar un titulo cabecero
+!
+print*, 'SUPERFICIE DE UN CIRCULO'
+!
+!peticion de los datos
+!
+print*, 'Introducir el valor del radio (m):'
+read*, radio
+!
+!algoritmo
+superficie=pi*radio**2
+!
+!visualizar resultado
+!
+print*, 'Para radio ', radio, ' la superficie es: ', superficie, 'm^{2}'
+end program circulo
