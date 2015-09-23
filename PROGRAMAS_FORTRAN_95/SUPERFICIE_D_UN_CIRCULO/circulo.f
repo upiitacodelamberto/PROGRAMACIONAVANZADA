67d5d05912a7a8c53609924b805b7dc42177cc5d
Lamberto <lamberto@RyRII>
2015-09-22 20:40:28 -0600

    (2015.09.22)Se agregaron 6 espacios para que el programa circulo.f se puediera compilara. Se hicieron algunos cambiios en la linea del ultimo print. Special thanks al Dr. Jesus Silva Lomeli por el aviso de que hay que escribir hasta la septima columna en el archivo fuente (circulo.f) . Segun parece esa es la regla en todos los archivos .f

--- PROGRAMAS_FORTRAN_95/SUPERFICIE_D_UN_CIRCULO/circulo.f
+++ PROGRAMAS_FORTRAN_95/SUPERFICIE_D_UN_CIRCULO/circulo.f
@@ -1,24 +1,26 @@
-program circulo
-!
-!programa para calcular areas de circulos
-!
-implicit none
-real::radio, superficie
-parameter (pi=3.1415926)
-!
-!visualizar un titulo cabecero
-!
-print*, 'SUPERFICIE DE UN CIRCULO'
-!
-!peticion de los datos
-!
-print*, 'Introducir el valor del radio (m):'
-read*, radio
-!
-!algoritmo
-superficie=pi*radio**2
-!
-!visualizar resultado
-!
-print*, 'Para radio ', radio, ' la superficie es: ', superficie, 'm^{2}'
-end program circulo
+      program circulo
+      !
+      !programa para calcular areas de circulos
+      !
+      !implicit none
+      real::radio, superficie
+      parameter (pi=3.1415926)
+      !
+      !visualizar un titulo cabecero
+      !
+      print*, 'SUPERFICIE DE UN CIRCULO'
+      !
+      !peticion de los datos
+      !
+      print*, 'Introducir el valor del radio (m):'
+      read*, radio
+      !
+      !algoritmo
+      superficie=pi*radio**2
+      !
+      !visualizar resultado
+      !
+      !print*, 'Para radio ', radio, ' la superficie es: ', superficie, 'm^{2}'
+      print*,'Para radio ', radio
+      print*,' la superficie es: ', superficie, 'm^{2}'
+      end program circulo
