cc77c8e55b56d03e98d356955c2701c9c39e6932
lambertomaza <lamberto9@gmail.com>
2018-06-29 13:18:56 -0500

    Add files via upload

---
+++ u1/Proyecto_Suma_Resta_Multiplicacion_Polinomios/Polinomio.h
@@ -0,0 +1,38 @@
+/** Polinomio.h - Una clase para representar polinomios.
+*/
+#include <iostream>		/* cout, ostream*/
+//#include <stdlib.h>	/* malloc() */
+using namespace std;
+#include "Rac.h"
+
+#ifndef Polinomio_h
+#define Polinomio_h
+#define maximo(a,b)	((a>b)?a:b)
+
+struct Polinomio {
+
+    Polinomio& operator+(Polinomio& PolObj);
+
+    Polinomio& operator-(Polinomio& PolObj);
+
+    Polinomio& operator*(Polinomio& PolObj);
+
+//    FcnDTrans& operator/(Polinomio& PolObj);
+
+    Polinomio& operator*(int factor);
+
+    friend ostream& operator<<(ostream& ostreamObj, Polinomio& PolObj);
+    
+    std::string string_show();
+
+    Polinomio(int intGrado, Rac * RacPt);
+    Polinomio(){ }
+    Polinomio(int){ }
+    /**Sobrecargar constructor de copia*/
+    Polinomio(Polinomio& P);    
+
+    int grado;
+    Rac * A;
+};//end struct Polinomio
+
+#endif // Polinomio_h
